b70bf0178d356ea7c91a1156bf02e5668e7c22ea
pabloalvareztoro <pabloalvareztoro@gmail.com>
2016-12-19 02:15:18 -0500

    Changed REST services URI to Heroku and added error handling for DB

--- app/controllers/LeagueController.scala
+++ app/controllers/LeagueController.scala
@@ -11,25 +11,26 @@ import play.modules.reactivemongo.json.collection.JSONCollection
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.bson.{BSONDocument, BSONObjectID}
 import api.JsonCombinators._
+import play.api.Play
 import play.modules.reactivemongo.json._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class LeagueController @Inject() (val ws: WSClient, val reactiveMongoApi: ReactiveMongoApi) extends Controller with MongoController with ReactiveMongoComponents {
+class LeagueController @Inject() (val ws: WSClient, val reactiveMongoApi: ReactiveMongoApi, val configuration: play.api.Configuration) extends Controller with MongoController with ReactiveMongoComponents {
 
-  def leagueCollection = reactiveMongoApi.db.collection[JSONCollection]("teams");
-  def fixtureCollection = reactiveMongoApi.db.collection[JSONCollection]("fixtures");
-  def resultCollection = reactiveMongoApi.db.collection[JSONCollection]("results");
+  def leagueCollection = db.collection[JSONCollection]("teams");
+  def fixtureCollection = db.collection[JSONCollection]("fixtures");
+  def resultCollection = db.collection[JSONCollection]("results");
 
   implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
 
   def createLeague = Action.async { implicit request =>
     val leagueId: String = BSONObjectID.generate.stringify
     val futureResponse: Future[WSResponse] = for {
-      league <- ws.url("http://localhost:3000/teamcreator/10").get()
-      saveLeague <- leagueCollection.insert(Json.obj("leagueId" -> leagueId, "league" -> league.json)).map { response =>
-        Created
-      }
+      league <- ws.url(configuration.underlying.getString("leaguemaker.teamscreator.uri") + "/teamcreator/10").get()
+      saveLeague <- leagueCollection.insert(Json.obj("leagueId" -> leagueId, "league" -> league.json))
+        .map {response => Created}
+        .recover {case _ => InternalServerError("DB Failure")}
     } yield league
     futureResponse.map { response =>
       Ok(Json.obj("leagueId" -> leagueId, "league" -> response.json))
@@ -42,10 +43,10 @@ class LeagueController @Inject() (val ws: WSClient, val reactiveMongoApi: Reacti
     val leagueId: String = (jsonRequest \ "leagueId").validate[String].get
     val league: JsValue = (jsonRequest \ "league").get
     val futureResponse: Future[WSResponse] = for {
-      fixture <- ws.url("http://localhost:3001/createfixture").post(league)
-      saveFixture <- fixtureCollection.insert(Json.obj("leagueId" -> leagueId, "fixtureId" -> fixtureId, "fixture" -> fixture.json)).map { response =>
-        Created
-      }
+      fixture <- ws.url(configuration.underlying.getString("leaguemaker.fixturecreator.uri") + "/createfixture").post(league)
+      saveFixture <- fixtureCollection.insert(Json.obj("leagueId" -> leagueId, "fixtureId" -> fixtureId, "fixture" -> fixture.json))
+        .map {response => Created}
+        .recover {case _ => InternalServerError("DB Failure")}
     } yield fixture
     futureResponse.map { response =>
       Ok(Json.obj("fixtureId" -> fixtureId, "leagueId" -> leagueId, "fixture" -> response.json))
@@ -58,10 +59,10 @@ class LeagueController @Inject() (val ws: WSClient, val reactiveMongoApi: Reacti
     val fixtureId: String = (jsonRequest \ "fixtureId").validate[String].get
     val leagueId: String = (jsonRequest \ "leagueId").validate[String].get
     val futureResponse: Future[WSResponse] = for {
-      fixtureResults <- ws.url("http://localhost:9001/fixtureresult").post(fixture)
-      saveFixture <- resultCollection.insert(Json.obj("leagueId" -> leagueId, "fixtureId" -> fixtureId, "fixture" -> fixtureResults.json)).map { response =>
-        Created
-      }
+      fixtureResults <- ws.url(configuration.underlying.getString("leaguemaker.matchresult.uri") + "/fixtureresult").post(fixture)
+      saveFixture <- resultCollection.insert(Json.obj("leagueId" -> leagueId, "fixtureId" -> fixtureId, "fixture" -> fixtureResults.json))
+        .map {response => Created}
+        .recover {case _ => InternalServerError("DB Failure")}
     } yield fixtureResults
     futureResponse.map { response =>
       Ok(Json.obj("leagueId" -> leagueId, "fixtureId" -> fixtureId, "results" -> response.json))
@@ -71,7 +72,7 @@ class LeagueController @Inject() (val ws: WSClient, val reactiveMongoApi: Reacti
   def playWeek = Action.async(parse.json) { implicit request =>
     val week: JsValue = request.body
     val futureResponse: Future[WSResponse] = for {
-      weekResults <- ws.url("http://localhost:9001/matchresult").post(week)
+      weekResults <- ws.url(configuration.underlying.getString("leaguemaker.matchresult.uri") + "/matchresult").post(week)
     } yield weekResults
     futureResponse.map { response =>
       Ok(Json.obj("results" -> response.json))
